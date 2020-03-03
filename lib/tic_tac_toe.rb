806ce38498b4b16bfc57c30ffb960d8a092a13a6
Elijah wines <elijahwines84@yahoo.com>
2020-03-03 15:46:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,6 +35,6 @@ class TicTacToe
   end
 
   def valid_move?(position_num)
-    position_num.between?(0,8) && !position_taken?(index)
+    position_num.between?(0,8) && !position_taken?(position_num)
   end
 end
