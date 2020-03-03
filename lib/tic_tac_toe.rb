2ba83117fe944b73102a4041b5a75db7b83c52d8
Elijah wines <elijahwines84@yahoo.com>
2020-03-03 16:13:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,7 +94,7 @@ class TicTacToe
 
   def winner
     WIN_COMBINATIONS.detect do |win_combo|
-      if (@board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
         return "X"
       elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "0" && (@board[win_combo[2]]) == "O"
         return "O"
